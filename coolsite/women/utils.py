1dc5cbd9f4294a521ce45100649aaf7788f8f421
Ivan1998-23 <birjukivan@gmail.com>
2022-12-06 17:43:32 +0200

    22. Включаем кэширование данных

--- coolsite/women/utils.py
+++ coolsite/women/utils.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-
+from django.core.cache import cache
 from .models import *
 
 menu = [{'title': 'О сайте', 'url_name': 'about'},
@@ -11,11 +11,17 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
 # Mixin позволяет создавать клас где у кажного обьекта бкдкь свои атрибуты
 
 class DataMixin:
-    paginate_by = 1
+    paginate_by = 3
     # Создает контекст для шаблона  по умолчанию
     def get_user_context(self, **kwargs):
         context = kwargs
-        cats = Category.objects.annotate(Count('women'))
+        # с помощю АРІ низкого уровня считываем с cats
+        cats = cache.get('cats')
+        # если cats небыл считан с БД то делаем запрос с БД
+        if not cats:
+            cats = Category.objects.annotate(Count('women'))
+            # заносим данные в кеш на 60 секунт
+            cache.set('cats', cats, 60)
 
         user_menu = menu.copy()
         if not self.request.user.is_authenticated:
